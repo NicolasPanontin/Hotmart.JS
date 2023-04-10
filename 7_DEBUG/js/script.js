99bd6f070fe24219975abfa2c5ca5aa7cce85c2c
NicolasPanontin <115738875+NicolasPanontin@users.noreply.github.com>
2023-04-10 11:32:29 -0300

    .

--- 7_DEBUG/js/script.js
+++ 7_DEBUG/js/script.js
@@ -51,3 +51,63 @@
 // if (c > 5) {
 //     c = 25;
 // }
+
+// 4 - tratamento de dados
+function checkNumber(n) {
+
+    const result = Number(n)
+
+    if(Number.isNaN(result)) {
+        console.log("Valor incorreto!");
+        return;
+    }
+
+    console.log("Valor correto!");
+    return result;
+}
+
+checkNumber(5);
+checkNumber("10")
+checkNumber({})
+checkNumber("teste")
+
+// 5 - Exceptions
+let x = 10
+
+if (x != 11 ) {
+    // throw new Error("O valor de x não pode ser diferente de 11")
+}
+
+// 6 - try catch
+try {
+    const soma = x + y;
+} catch(error) {
+    console.log(`Erro no programa: ${error}`)
+}
+
+// 7 - finally
+try {
+    const value = checkNumber("1")
+
+    if(!value) {
+        throw new Error("Valores inválidos")
+    }
+} catch (error) {
+    console.log(`Opa , aconteceu um problema: ${error}`);
+} finally {
+    console.log("O códogo foi executado")
+}
+
+// 8 - assertions
+function checkArray(arr) {
+
+    if(arr.length === 0) {
+        throw new Error("O array precisa ter elementos")
+    } else {
+        console.log(`O array tem ${arr.length} elementos`)
+    }
+}
+
+// checkArray([]);
+
+checkArray([1, 2, 3]);
