bc6482c779f2511779172c778ad579243beb7a1e
NicolasPanontin <115738875+NicolasPanontin@users.noreply.github.com>
2023-04-17 11:46:01 -0300

    17/04

--- 10_EVENTOS/js/script.js
+++ 10_EVENTOS/js/script.js
@@ -43,3 +43,70 @@ btnInsideContainer.addEventListener("click", (e) => {
     e.stopPropagation();
     console.log("Evento 2");
 })
+
+// 5 - removendo evento padrão
+const a = document.querySelector("a")
+
+a.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    console.log("Não alteroua a página")
+})
+
+// 6 - eventos tecla
+document.addEventListener("keyup", (e)=> {
+    console.log(`Soltou a tecla ${e.key}`);
+})
+
+document.addEventListener("keydown", (e)=> {
+    console.log(`Apertou a tecla ${e.key}`);
+})
+
+// 7 - eventos de mouse
+const mouseEvents = document.querySelector("#mouse")
+
+mouseEvents.addEventListener("mousedown",  () => {
+    console.log("Pressionou o botão");
+})
+
+mouseEvents.addEventListener("mouseup",  () => {
+    console.log("Soltou o botão");
+});
+
+mouseEvents.addEventListener("dblclick",  () => {
+    console.log("click duplo");
+});
+
+// 8 - movimento do mouse
+document.addEventListener("mousemove", (e) =>{
+    // console.log(`No eixo x: ${e.x}`)
+    // console.log(`No eixo y: ${e.y}`)
+})
+
+// 9 - evento de scroll
+window.addEventListener("scroll", (e) => {
+    if(window.pageYOffset > 200) {
+        console.log("passamos de 200px")
+    }
+})
+
+// 10 - evento de focus/blur
+const input = document.querySelector("#my-input")
+
+input.addEventListener("focus", (e) => {
+    console.log("Entrou no input")
+})
+
+input.addEventListener("blur", (e) => {
+    console.log("Saiu do input")
+})
+
+// 11 - evento de carregamento
+window.addEventListener("load", () => {
+    console.log("A pagina carregou!")
+});
+
+window.addEventListener("beforeunload", (e) => {
+    e.preventDefault()
+    e.returnValue = "";
+})
