d30925db65b79658141a6f0ac49ac365d5e3a045
NicolasPanontin <115738875+NicolasPanontin@users.noreply.github.com>
2023-04-18 12:02:29 -0300

    18/04

--- 10_EVENTOS/js/script.js
+++ 10_EVENTOS/js/script.js
@@ -110,3 +110,25 @@ window.addEventListener("beforeunload", (e) => {
     e.preventDefault()
     e.returnValue = "";
 })
+
+// 12 - debounce
+const debounce = (f, delay) => {
+    let timeout;
+  
+    return (...arguments) => {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+  
+      timeout = setTimeout(() => {
+        f.apply(arguments);
+      }, delay);
+    };
+  };
+  
+  window.addEventListener(
+    "mousemove",
+    debounce(() => {
+      console.log("Executando a cada 400ms");
+    }, 400)
+  );
---
+++ 11_JS_MODERNO/js/script.js
@@ -0,0 +1,71 @@
+// 1 - var, let e const
+var x = 10
+var y = 15
+
+if(y > 10) {
+    var x = 5
+    console.log(x)
+}
+
+console.log(x)
+
+let a = 10
+let b = 15
+
+if(b > 10) {
+    let a = 5;
+    console.log(a)
+}
+
+console.log(a);
+
+function lobName() {
+    const name = "Redro";
+    console.log(name);
+}
+
+const name = "Matheus";
+
+lobName();
+
+console.log(name);
+
+// 2 - arrow function
+const sum = function (a, b) {
+    return a + b;
+  };
+  
+  const arrowSum = (a, b) => a + b;
+  
+  console.log(sum(5, 5));
+  console.log(arrowSum(5, 5));
+  
+  const greeting = (name) => {
+    if (name) {
+      return "Hello " + name + "!";
+    } else {
+      return "Hello!";
+    }
+  };
+  
+  console.log(greeting());
+  console.log(greeting("Matheus"));
+  
+  const user = {
+    name: "Theo",
+    sayUserName() {
+      setTimeout(function () {
+        console.log(this);
+        console.log("Username: " + this.name);
+      }, 1000);
+    },
+    sayUserNameArrow() {
+      setTimeout(() => {
+        console.log(this);
+        console.log("Username: " + this.name);
+      }, 1200);
+    },
+  };
+  
+  // user.sayUserName()
+  // user.sayUserNameArrow()
