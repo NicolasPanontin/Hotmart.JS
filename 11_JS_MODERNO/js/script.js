f941fcad733474cb9025c736a9f6fa4ec57c5c57
NicolasPanontin <115738875+NicolasPanontin@users.noreply.github.com>
2023-04-20 09:57:33 -0300

    20/04

--- 11_JS_MODERNO/js/script.js
+++ 11_JS_MODERNO/js/script.js
@@ -1,71 +1,196 @@
-// 1 - var, let e const
-var x = 10
-var y = 15
+// // 1 - var, let e const
+// var x = 10
+// var y = 15
 
-if(y > 10) {
-    var x = 5
-    console.log(x)
+// if(y > 10) {
+//     var x = 5
+//     console.log(x)
+// }
+
+// console.log(x)
+
+// let a = 10
+// let b = 15
+
+// if(b > 10) {
+//     let a = 5;
+//     console.log(a)
+// }
+
+// console.log(a);
+
+// function lobName() {
+//     const name = "Redro";
+//     console.log(name);
+// }
+
+// const name = "Matheus";
+
+// lobName();
+
+// console.log(name);
+
+// // 2 - arrow function
+// const sum = function (a, b) {
+//     return a + b;
+//   };
+  
+//   const arrowSum = (a, b) => a + b;
+  
+//   console.log(sum(5, 5));
+//   console.log(arrowSum(5, 5));
+  
+//   const greeting = (name) => {
+//     if (name) {
+//       return "Hello " + name + "!";
+//     } else {
+//       return "Hello!";
+//     }
+//   };
+  
+//   console.log(greeting());
+//   console.log(greeting("Matheus"));
+  
+//   const user = {
+//     name: "Theo",
+//     sayUserName() {
+//       setTimeout(function () {
+//         console.log(this);
+//         console.log("Username: " + this.name);
+//       }, 1000);
+//     },
+//     sayUserNameArrow() {
+//       setTimeout(() => {
+//         console.log(this);
+//         console.log("Username: " + this.name);
+//       }, 1200);
+//     },
+//   };
+  
+//   // user.sayUserName()
+//   // user.sayUserNameArrow()
+
+// // 3 - filter
+// const arr = [1, 2, 3, 4, 5, 6];
+
+// const highNumbers = arr.filter((n) => {
+//   if (n >= 3) {
+//     return n;
+//   }
+// });
+
+// console.log(highNumbers);
+
+// const users = [
+//   {name: "Nicolas", available: true},
+//   {name: "João", available: false},
+//   {name: "Maria", available: true},
+//   {name: "Josias", available: false},
+//   {name: "Kleber", available: true},
+// ]
+
+// const availableUsers =  users.filter((user) => user.available)
+
+// console.log(availableUsers);
+
+// 4 - map
+const products = [
+  { name: "Camisa", price: 10.99, category: "Roupa" },
+  { name: "Chaleira elétrica", price: 150, category: "Eletro" },
+  { name: "Fogão", price: 499, category: "Eletro" },
+  { name: "Calça jeans", price: 49.99, category: "Roupa" },
+];
+
+products.map((product) => {
+  if (product.category === "Roupa") {
+    product.onSale = true;
+  }
+});
+
+console.log(products);
+
+// 5 - template literals
+const userName = "Nicolas"
+const age = 18;
+
+console.log(`O nome do usuário e ${userName} e ele tem ${age} anos`)
+
+// 6 - destructuring
+const fruits = ["Maçã", "Mamão", "Laranja"]
+
+const [f1, f2, f3]= fruits
+
+console.log(f1, f3);
+
+const productDetails = {
+  name: "Mouse",
+  price: 39.99,
+  category: "Periférico", 
+  color: "Cinza",
+};
+
+const {name: productName, price, category, color} = productDetails
+
+console.log(`O nome do pruduto é ${productName}, ele custa R$${price} e é da cor ${color}`);
+
+// 7 spread operator
+const a1 = [1, 2, 3];
+const a2 = [4, 5, 6];
+
+const a3 = [...a1, ...a2];
+
+console.log(a3);
+
+const a4 = [0, ...a1, 4];
+
+console.log(a4);
+
+const carName = { name: "Gol" };
+const carBrand = { brand: "VW" };
+const otherInfos = { km: 100000, price: 49000 };
+
+const car = { ...carName, ...carBrand, ...otherInfos };
+
+// 8 - classes
+class Product {
+  constructor(name, price) {
+    this.name = name
+    this.price = price
+  }
+
+  productWithDiscount(discount) {
+    return this.price * ((100 - discount) / 100);
+  }
 }
 
-console.log(x)
+const shirt = new Product ("Camisa gola V", 20);
 
-let a = 10
-let b = 15
+console.log(shirt);
 
-if(b > 10) {
-    let a = 5;
-    console.log(a)
+console.log(shirt.productWithDiscount(10));
+console.log(shirt.productWithDiscount(15));
+
+// 9 heranca
+class ProductWithAttributes extends Product {
+  constructor(name, price, colors) {
+    super(name, price);
+    this.colors = colors;
+  }
+
+  showColors() {
+    console.log("As cores são: ");
+    this.colors.forEach((color) => {
+      console.log(color);
+    });
+  }
 }
 
-console.log(a);
+const hat = new ProductWithAttributes("Chapéu", 29.99, [
+  "Preto",
+  "Amarelo",
+  "Verde",
+]);
 
-function lobName() {
-    const name = "Redro";
-    console.log(name);
-}
+console.log(hat.name);
 
-const name = "Matheus";
-
-lobName();
-
-console.log(name);
-
-// 2 - arrow function
-const sum = function (a, b) {
-    return a + b;
-  };
-  
-  const arrowSum = (a, b) => a + b;
-  
-  console.log(sum(5, 5));
-  console.log(arrowSum(5, 5));
-  
-  const greeting = (name) => {
-    if (name) {
-      return "Hello " + name + "!";
-    } else {
-      return "Hello!";
-    }
-  };
-  
-  console.log(greeting());
-  console.log(greeting("Matheus"));
-  
-  const user = {
-    name: "Theo",
-    sayUserName() {
-      setTimeout(function () {
-        console.log(this);
-        console.log("Username: " + this.name);
-      }, 1000);
-    },
-    sayUserNameArrow() {
-      setTimeout(() => {
-        console.log(this);
-        console.log("Username: " + this.name);
-      }, 1200);
-    },
-  };
-  
-  // user.sayUserName()
-  // user.sayUserNameArrow()
+hat.showColors();
