c28450f8e986b4b56d8f0baab2feb3425b43903f
NicolasPanontin <115738875+NicolasPanontin@users.noreply.github.com>
2023-05-09 11:07:42 -0300

    09/05

---
+++ 12_AXIOS/js/interceptors.js
@@ -0,0 +1,21 @@
+// Requisição
+axios.interceptors.request.use(
+    function(config) {
+        console.log("Antes da requisição...")
+        return config;
+    },
+    function(error) {
+        return Promise.reject(error)
+    }
+);
+
+// Resposta
+postsFetch.interceptors.response.use(
+    function(response) {
+        console.log("Antes da requisição...")
+        return response;
+    },
+    function(error) {
+        return Promise.reject(error)
+    }
+)
